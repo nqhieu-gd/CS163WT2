a0587e94912353b83ef08b8d0b88078b677ebb82
Lilith-51 <161232064+Lilith-51@users.noreply.github.com>
2026-01-21 08:35:54 +0700

    Self-Update

---
+++ W1/Lab Class/README.md
@@ -0,0 +1,3 @@
+Week 1
+Assignment 3
+AI usage: for giving example and checking the self-code, as well as validating the time-space complexity documentation
---
+++ W2/Theory Class/L1/QueenlyMarjesty.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+/*
+//Non-recursion
+bool PlaceQueen(int m, int p, int n, int* ar) {
+    if (ar[m*n + p] == -1) return 0;
+    ar[m*n + p] = 1;
+    for (int i = 0; i < n; i++) {
+        if (ar[i*n + p] == -1) return 0;
+        if (ar[m*n + i] == -1) return 0;
+    }
+    for (int i = 0; i < n; i++) {
+        if (i != m) ar[i*n + p] = -1;
+        if (i != p) ar[m*n + i] = -1;
+    }
+    return 1;
+}
+
+void NumOfQueens(int n) {
+    int* ar = new int[n*n];
+    int k = 0;
+    for (int i = 0; i < n; i++) {
+        if (PlaceQueen(k, i, n, ar)) {
+            k++;
+            continue;
+        }
+        else k++;
+    }
+}*/
+
+//Recursion
+void ReTableUp(int* ar, int n) {
+    int* br = new int[(n + 1)*(n + 1)];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            br[i*(n + 1) + j] = ar[i*n + j];
+        }
+    }
+    delete[] ar;
+    ar = br;
+}
+
+void PlaceQueen(int n, int* ar) {
+    if (n == 1) {
+        ar = new int[1];
+        ar[1] = 1;
+        return;
+    }
+}
+
+int CalcQueen(int* ar, int n) {
+    int num = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (ar[i*n + j] == 1) {
+                num++;
+                break;
+            }
+        }
+    }
+    return num;
+}
+
+void DisplayQueen(int* ar, int n) {
+    std::cout << "_";
+    for (int i = 0; i< n; i++) std::cout << "____";
+    for (int i = 0; i < n; i++) {
+        std::cout << '|';
+        for (int j = 0; j < n; j++) {
+            if (ar[i*n + j] != 1) {
+                std::cout << "   |";
+            }
+            else std::cout << " Q |";
+        }
+        std::cout << "_";
+        for (int i = 0; i< n; i++) std::cout << "____";
+    }
+    delete[] ar;
+}
